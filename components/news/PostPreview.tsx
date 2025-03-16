0a1f338b061d03bdc2329619db6a15dce1de77ec
weeelb <harry.tech2001@gmail.com>
2025-03-16 22:30:28 +0900

    fix news

--- components/news/PostPreview.tsx
+++ components/news/PostPreview.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
-import { PostMetadata } from "./PostMetadata";
 
-const PostPreview = (props: PostMetadata) => {
+interface PostPreviewProps {
+    slug: string;
+    title: string;
+    date: string;
+    subtitle?: string;
+}
+
+const PostPreview = ({ slug, title, date }: PostPreviewProps) => {
     return (
-        <div
-            className="flex flex-col gap-2 py-6 md:gap-6 md:flex-row md:items-center px-4"
-        >
-            <p className="w-32 md:text-lg text-gray-300 md:text-right shrink-0">{props.date}</p>
-
-            <Link href={`news/${props.slug}`} className='hover:underline'>
-                <h2 className="md:text-lg md:font-semibold text-white">{props.title}</h2>
+        <div className="flex flex-col gap-2 py-6 md:gap-6 md:flex-row md:items-center px-4">
+            <p className="w-32 md:text-lg text-gray-300 md:text-right shrink-0">{date}</p>
+            <Link href={`news/${slug}`} className='hover:underline'>
+                <h2 className="md:text-lg md:font-semibold text-white">{title}</h2>
             </Link>
         </div>
     );
